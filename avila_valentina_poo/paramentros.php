9a347ee1bd11306a00c6087b45a6f5e95bce1624
valentina <valeavila717@gmail.com>
2023-10-19 17:38:51 -0500

    parametros

---
+++ avila_valentina_poo/paramentros.php
@@ -0,0 +1,13 @@
+<?php
+function metodo1($nombre, $saludo, $pregunta, $despedida) {
+    echo   "$saludo $nombre, $pregunta, $despedida.";
+}
+
+metodo1(saludo: "Buenos dias", nombre: "Valentina", pregunta: "¿Como estas?",  despedida:"Adios");
+
+
+function metodo2($nombre, $edad = "20", $estatura = "1.80", $profesion = "Programador") {
+    echo '<br>' . "$nombre $profesion $edad $estatura ";
+}
+
+metodo2("Juan"); 
